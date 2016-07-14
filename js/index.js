e8e2d03625a2931c347c2e7c2b11c33273ff9a6b
Anup Parekh <anup@anup-parekhs-imac.local>
2016-07-14 16:35:53 +0100

    comment

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-
-var level1 = "<div class=\"tile tile-position-1-1 \">5</div><div class=\"tile tile-position-1-2 \">7</div><div class=\"tile tile-position-2-1 \">9</div><div class=\"tile tile-position-2-2 \">3</div>";
-
-var level2 = "<div class=\"tile tile-position-1-1 \">3</div><div class=\"tile tile-position-1-2 \">2</div><div class=\"tile tile-position-1-3 \">1</div><div class=\"tile tile-position-2-1 \">5</div><div class=\"tile tile-position-2-2 \">4</div><div class=\"tile tile-position-2-3 \">3</div><div class=\"tile tile-position-3-1 \">2</div><div class=\"tile tile-position-3-2 \">1</div><div class=\"tile tile-position-3-3 \">5</div>";
-
-var level3 = "<div class=\"tile tile-position-1-1 \">5</div><div class=\"tile tile-position-1-2 \">7</div><div class=\"tile tile-position-1-3 \">1</div><div class=\"tile tile-position-1-4 \">6</div><div class=\"tile tile-position-2-1 \">9</div><div class=\"tile tile-position-2-2 \">3</div><div class=\"tile tile-position-2-3 \">4</div><div class=\"tile tile-position-2-4 \">1</div><div class=\"tile tile-position-3-1 \">4</div><div class=\"tile tile-position-3-2 \">6</div><div class=\"tile tile-position-3-3 \">3</div><div class=\"tile tile-position-3-4 \">7</div><div class=\"tile tile-position-4-1 \">8</div><div class=\"tile tile-position-4-2 \">5</div><div class=\"tile tile-position-4-3 \">9</div><div class=\"tile tile-position-4-4 \">2</div>";
-
-var level4 = "<div class=\"tile tile-position-1-1 \">66</div><div class=\"tile tile-position-1-2 \">7</div><div class=\"tile tile-position-1-3 \">36</div><div class=\"tile tile-position-1-4 \">2</div><div class=\"tile tile-position-1-5 \">13</div><div class=\"tile tile-position-2-1 \">21</div><div class=\"tile tile-position-2-2 \">19</div><div class=\"tile tile-position-2-3 \">5</div><div class=\"tile tile-position-2-4 \">58</div><div class=\"tile tile-position-2-5 \">1</div><div class=\"tile tile-position-3-1 \">5</div><div class=\"tile tile-position-3-2 \">9</div><div class=\"tile tile-position-3-3 \">8</div><div class=\"tile tile-position-3-4 \">17</div><div class=\"tile tile-position-3-5 \">3</div><div class=\"tile tile-position-4-1 \">18</div><div class=\"tile tile-position-4-2 \">6</div><div class=\"tile tile-position-4-3 \">45</div><div class=\"tile tile-position-4-4 \">29</div><div class=\"tile tile-position-4-5 \">72</div><div class=\"tile tile-position-5-1 \">12</div><div class=\"tile tile-position-5-2 \">88</div><div class=\"tile tile-position-5-3 \">4</div><div class=\"tile tile-position-5-4 \">22</div><div class=\"tile tile-position-5-5 \">10</div>";
-
 var gameExplanation = "<strong class=\"important\">How to play </strong> swipe tiles to subtract from each other. End up with 0 to get to next level";
 
 var currentLevel;
@@ -27,7 +18,7 @@ function addBoard() {
 	$('.levels').css('display','none');
 	$('.control-container').css('display','block');	
 	$('.game-container').css('display','block');
-	$('.score-container').html('0');
+	$('.score-container span').html('0');
 	
 	var size = Math.sqrt(levels[currentLevel].length); 
 	
@@ -52,7 +43,7 @@ function generateLevel(size) {
 
     $(function() { 
     
-    $("#levelSelector").on('click', function(e) {
+    $(".level-selector").on('click', function(e) {
     	e.preventDefault();
 		$('.control-container').css('display','none');	
 		$('.game-container').css('display','none');
@@ -70,7 +61,7 @@ function generateLevel(size) {
 	
 	$('.restart-container').on('click', function(e) {
 		e.preventDefault();
-		$('.score-container').html('0');
+		$('.score-container span').html('0');
 		var size = Math.sqrt(levels[currentLevel].length);
 		$('.tile-container').html(generateLevel(size));
 		addSwipeTo('.tile');
@@ -127,7 +118,7 @@ function generateLevel(size) {
 					$('.'+newClassIndex).remove();
 					$('.tile-container').append("<div class=\"tile "+newClassIndex+" tile-tick tile-complete\"></div>");
 				}
-				$('.score-container').html(parseInt($('.score-container').html(), 10)+1);
+				$('.score-container span').html(parseInt($('.score-container span').html(), 10)+1);
 			}
         },
          threshold:5
