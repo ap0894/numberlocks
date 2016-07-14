7d79cf66397a8aedcc228abefec6f10a5859a8c3
Anup Parekh <anup@anup-parekhs-imac.local>
2016-07-14 19:21:46 +0100

    comment

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var gameExplanation = "<strong class=\"important\">How to play </strong> swipe tiles to subtract from each other. End up with 0 to get to next level";
 
 var currentLevel;
+var remainingTiles;
+var completeBonus = 500;
+var moves = 0;
 
 function createGrid(size) {
 	var grid = "";
@@ -15,10 +18,11 @@ function createGrid(size) {
 }
 
 function addBoard() {
+	moves = 0;
 	$('.levels').css('display','none');
 	$('.control-container').css('display','block');	
 	$('.game-container').css('display','block');
-	$('.score-container span').html('0');
+	$('.moves-container span').html(moves);
 	
 	var size = Math.sqrt(levels[currentLevel].length); 
 	
@@ -41,7 +45,24 @@ function generateLevel(size) {
 	return output;
 }
 
+function calculateScore() {
+	var score = 1000 + completeBonus + (((levels[currentLevel].length-1)-moves) * 75);
+	return score;
+}
+
+function incrementLevel() {
+	var newNum = parseInt(currentLevel.substr(5),10)+1;
+	return "level"+newNum;
+}
+
     $(function() { 
+    
+    $(".close").on('click', function(e) {
+    	gameOverModal.style.display = "none";
+		remainingTiles = levels[currentLevel].length;
+		addBoard();	
+		addSwipeTo('.tile');
+	});
     
     $(".level-selector").on('click', function(e) {
     	e.preventDefault();
@@ -55,17 +76,22 @@ function generateLevel(size) {
 	$("#level1, #level2, #level3, #level4, #level5, #level6, #level7, #level8, #level9, #level10").on('click', function(e) {
 		e.preventDefault();
 		currentLevel = $(this).attr('id');
+		remainingTiles = levels[currentLevel].length;
 		addBoard();	
 		addSwipeTo('.tile');
 	});
 	
 	$('.restart-container').on('click', function(e) {
 		e.preventDefault();
-		$('.score-container span').html('0');
 		var size = Math.sqrt(levels[currentLevel].length);
+		remainingTiles = levels[currentLevel].length;
 		$('.tile-container').html(generateLevel(size));
 		addSwipeTo('.tile');
+		moves=0;
+		$('.moves-container span').html(moves);
 	});     
+	
+		//var remainingTiles = levels[currentLevel].length;
       //Enable swiping...
       var addSwipeTo = function(selector) {
       $(selector).swipe("destroy");
@@ -109,7 +135,8 @@ function generateLevel(size) {
 			break;
 			default:
 			} 
-			if(move) {			
+			if(move) {
+			moves++;			
 			newClassIndex = 'tile-position-'+x+'-'+y;
 				if ($('.'+newClassIndex).text()) {
 					newValue = Math.abs((parseInt($(this).text(),10) - parseInt($('.'+newClassIndex).text(),10)));
@@ -120,11 +147,20 @@ function generateLevel(size) {
 					$('.tile-container').append("<div class=\"tile "+classIndex+" tile-tick tile-complete\"></div>");
 					if(newValue !=0){
 						$(this).text(newValue);
+						remainingTiles--;
 					} else {
 						$('.'+newClassIndex).remove();
 						$('.tile-container').append("<div class=\"tile "+newClassIndex+" tile-tick tile-complete\"></div>");
+						remainingTiles--;
+						remainingTiles--;
 					}
-					$('.score-container span').html(parseInt($('.score-container span').html(), 10)+1);
+					$('.moves-container span').html(moves);
+				}
+				if(remainingTiles === 0) {
+					var score = calculateScore();
+					gameOverModal.style.display = "block";
+					$('#score').html(score);
+					currentLevel = incrementLevel();
 				}
 			}
         },
